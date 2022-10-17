487f30c20bd77162a356c7ca0babfd37dd5cc34a
Jiyaul Haque Shekh Gaffar <jiyaulhaque.shekhgaffar@Impigertech.com>
2022-10-17 18:55:46 +0530

    Add project files.

---
+++ expense-tracking/Models/ApplicationDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace expense_tracking.Models
+{
+    public class ApplicationDB: DbContext
+    {
+        public ApplicationDB(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+       public DbSet<Category> Categories { get; set; }
+    }
+}
